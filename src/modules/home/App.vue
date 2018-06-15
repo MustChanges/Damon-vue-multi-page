51ab009f75c5422af84d9532f5c3270ba26216c4
KF@DESKTOP-HPGI67P <819807012@qq.com>
2018-06-15 10:59:21 +0800

    上传完整项目

---
+++ src/modules/home/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>这里是首页</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data: function() {	
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
